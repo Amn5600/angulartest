77fa02be9eefc6842b12b669cb4dd85280e9aa72
Amandeep Rawat <amandeep.rawat@nagarro.com>
2021-04-05 10:30:47 +0530

    fixes

--- src/app/nutrition-analysis/ingredients-result/ingredients-result.component.ts
+++ src/app/nutrition-analysis/ingredients-result/ingredients-result.component.ts
@@ -13,26 +13,28 @@ import { DataService } from '../../shared/services/data.service';
 export class IngredientsResultComponent implements OnInit {
   constructor(private dataService: DataService, private router: Router) { }
 
-  nutrition = [];
+  nutrition = {};
   showNutrition = false;
   showError = false;
- 
 
 
- 
+
+
 
   ngOnInit(): void {
     this.showNutrition = true;
-    this.nutrition = this.dataService['data'];
-    console.log(this.nutrition)
+    this.nutrition = { ingredients: this.dataService['data']['ingredients'] || [] };
 
     //redirect if no result
-    if (!this.nutrition['ingredients']) {
-      this.router.navigate(['/nutrition'])
+    if (!this.nutrition['ingredients'].length) {
+      this.router.navigate(['/nutrition']);
     };
 
-    if (this.nutrition['ingredients'][0]['parsed'] == undefined) {
-      this.showError = true
+    if (this.nutrition['ingredients'].length) {
+      if (this.nutrition['ingredients'][0]['parsed'] == undefined) {
+        this.showError = true;
+
+      }
     }
   }
 
